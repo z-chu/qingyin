a32e16ddba55c702662aff48387c6aa9d3cd8f60
zchu <zchu8073@gmail.com>
2019-01-25 15:15:36 +0800

    Update

--- common/src/main/java/com/github/zchu/common/anim/AnimatorKt.kt
+++ common/src/main/java/com/github/zchu/common/anim/AnimatorKt.kt
@@ -1,57 +1,50 @@
-package com.cysion.targetfun
+package com.github.zchu.common.anim
 
 import android.animation.Animator
 import android.view.ViewPropertyAnimator
 
-class AnimatorListenerObj : Animator.AnimatorListener {
+class AnimatorListenerBridge : Animator.AnimatorListener {
 
-    //-----------------
-    private var _a: ((animation: Animator?) -> Unit)? = null
-
-    fun _onAnimationRepeat(t: ((animation: Animator?) -> Unit)) {
-        _a = t
-    }
-
-    override fun onAnimationRepeat(animation: Animator?) {
-        _a?.invoke(animation)
-    }
-
-    //------------------
-    private var _b: ((animation: Animator?) -> Unit)? = null
-
-    fun _onAnimationEnd(t: ((animation: Animator?) -> Unit)) {
-        _b = t
-    }
-
-    override fun onAnimationEnd(animation: Animator?) {
-        _b?.invoke(animation)
-    }
-
-    //--------------------
-    private var _c: ((animation: Animator?) -> Unit)? = null
-
-    fun _onAnimationCancel(t: ((animation: Animator?) -> Unit)) {
-        _c = t
-    }
-
-    override fun onAnimationCancel(animation: Animator?) {
-        _c?.invoke(animation)
-    }
-
-    //---------------------
-    private var _d: ((animation: Animator?) -> Unit)? = null
+    private var _onAnimationStart: ((animation: Animator?) -> Unit)? = null
+    private var _onAnimationEnd: ((animation: Animator?) -> Unit)? = null
+    private var _onAnimationCancel: ((animation: Animator?) -> Unit)? = null
+    private var _onAnimationRepeat: ((animation: Animator?) -> Unit)? = null
 
     fun _onAnimationStart(t: ((animation: Animator?) -> Unit)) {
-        _d = t
+        _onAnimationStart = t
+    }
+
+    fun _onAnimationEnd(t: ((animation: Animator?) -> Unit)) {
+        _onAnimationEnd = t
+    }
+
+    fun _onAnimationCancel(t: ((animation: Animator?) -> Unit)) {
+        _onAnimationCancel = t
+    }
+
+    fun _onAnimationRepeat(t: ((animation: Animator?) -> Unit)) {
+        _onAnimationRepeat = t
     }
 
     override fun onAnimationStart(animation: Animator?) {
-        _d?.invoke(animation)
+        _onAnimationStart?.invoke(animation)
+    }
+
+    override fun onAnimationEnd(animation: Animator?) {
+        _onAnimationEnd?.invoke(animation)
+    }
+
+    override fun onAnimationCancel(animation: Animator?) {
+        _onAnimationCancel?.invoke(animation)
+    }
+
+    override fun onAnimationRepeat(animation: Animator?) {
+        _onAnimationRepeat?.invoke(animation)
     }
 }
 
-inline fun ViewPropertyAnimator._setListener(func: (AnimatorListenerObj.() -> Unit)) =
-    setListener(AnimatorListenerObj().apply(func))
+inline fun ViewPropertyAnimator._setListener(func: (AnimatorListenerBridge.() -> Unit)) =
+    setListener(AnimatorListenerBridge().apply(func))
 
-inline fun Animator._addListener(func: (AnimatorListenerObj.() -> Unit)) =
-    addListener(AnimatorListenerObj().apply(func))
+inline fun Animator._addListener(func: (AnimatorListenerBridge.() -> Unit)) =
+    addListener(AnimatorListenerBridge().apply(func))
