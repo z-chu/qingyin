4ce083e99b4fa1bc86ab77ba2fa34ca779aa44c8
zchu <zchu8073@gmail.com>
2019-02-12 13:55:10 +0800

    Update

--- app/src/main/java/live/qingyin/talk/TestActivity.java
+++ app/src/main/java/live/qingyin/talk/TestActivity.java
@@ -1,15 +1,15 @@
 package live.qingyin.talk;
 
 import android.os.Bundle;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.constraintlayout.widget.ConstraintLayout;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class TestActivity extends AppCompatActivity {
 
-    private ConstraintLayout container;
+    private LinearLayout container;
     private TextView message;
     private BottomNavigationView navigation;
 
