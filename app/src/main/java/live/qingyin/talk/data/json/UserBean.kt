7925c67ecfee1dddbae17394f044a98aa75ab61d
zchu <zchu8073@gmail.com>
2019-03-14 11:07:38 +0800

    Rename

--- app/src/main/java/live/qingyin/talk/data/json/UserBean.kt
+++ app/src/main/java/live/qingyin/talk/data/json/UserBean.kt
@@ -1,10 +1,12 @@
 package live.qingyin.talk.data.json
 
+import live.qingyin.talk.usersession.model.UserSession
+
 data class UserBean(val objectId: String, val username: String, val sessionToken: String) {
     var phone: String? = null
 
 }
 
-fun UserBean.toUser(): live.qingyin.talk.usersession.model.UserSession {
-    return live.qingyin.talk.usersession.model.UserSession(objectId, username, sessionToken, phone)
+fun UserBean.toUser(): UserSession {
+    return UserSession(objectId, username, sessionToken, phone)
 }
