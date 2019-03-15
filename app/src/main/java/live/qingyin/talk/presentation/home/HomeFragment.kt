82ba5470337e620d81a9d6807ee04e1fd661b981
zchu <zchu8073@gmail.com>
2019-03-15 22:19:26 +0800

    添加主界面代码

--- app/src/main/java/live/qingyin/talk/presentation/home/HomeFragment.kt
+++ app/src/main/java/live/qingyin/talk/presentation/home/HomeFragment.kt
@@ -1,3 +1,14 @@
 package live.qingyin.talk.presentation.home
 
-class HomeFragment
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import live.qingyin.talk.R
+
+class HomeFragment : Fragment() {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+}
--- app/src/main/java/live/qingyin/talk/presentation/main/MainActivity.kt
+++ app/src/main/java/live/qingyin/talk/presentation/main/MainActivity.kt
@@ -3,11 +3,14 @@ package live.qingyin.talk.presentation.main
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import androidx.annotation.IdRes
 import androidx.appcompat.app.AppCompatActivity
-import com.github.zchu.common.help.showToastShort
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import kotlinx.android.synthetic.main.activity_main.*
 import live.qingyin.talk.R
+import live.qingyin.talk.presentation.home.HomeFragment
 import live.qingyin.talk.presentation.login.LoginActivity
 import live.qingyin.talk.presentation.user.MeFragment
 import live.qingyin.talk.usersession.UserSessionManager
@@ -17,31 +20,37 @@ class MainActivity : AppCompatActivity() {
 
     private val userManager: UserSessionManager by inject()
 
+    private val fragmentFunc: (tag: String) -> Fragment = {
+        val tagId = it.toInt()
+        when (tagId) {
+            R.id.navigation_home -> HomeFragment()
+            R.id.navigation_following -> MeFragment()
+            R.id.navigation_messages -> MeFragment()
+            R.id.navigation_me -> MeFragment()
+            else -> error("没有给 fragment tag：$it 返回对应的 Fragment 实例。")
+        }
+    }
+
 
     private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
-        when (item.itemId) {
+        val itemId = item.itemId
+        val supportFragmentManager = supportFragmentManager
+        when (itemId) {
             R.id.navigation_home -> {
-                showToastShort(R.string.title_home)
+                supportFragmentManager.selectFragmentDisplay(containerViewId, itemId.toString(), fragmentFunc)
                 return@OnNavigationItemSelectedListener true
             }
-            R.id.navigation_following -> {
-                return@OnNavigationItemSelectedListener checkLoggedIn {
-                    showToastShort(R.string.title_following)
-                }
-            }
-            R.id.navigation_messages -> {
-                return@OnNavigationItemSelectedListener checkLoggedIn {
-                    showToastShort(R.string.title_messages)
-                }
-            }
+            R.id.navigation_following,
+            R.id.navigation_messages,
             R.id.navigation_me -> {
                 return@OnNavigationItemSelectedListener checkLoggedIn {
-                    showToastShort(R.string.title_me)
+                    supportFragmentManager.selectFragmentDisplay(containerViewId, itemId.toString(), fragmentFunc)
                 }
             }
         }
         false
     }
+
 
     private inline fun checkLoggedIn(block: () -> Unit): Boolean {
         return if (userManager.isLoggedIn()) {
@@ -57,16 +66,34 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        supportFragmentManager.selectFragmentDisplay(containerViewId, R.id.navigation_home.toString(), fragmentFunc)
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
-
-        supportFragmentManager
-                .beginTransaction()
-                .replace(R.id.container, MeFragment())
-                .commit()
-
     }
 
+    private fun FragmentManager.selectFragmentDisplay(
+        @IdRes containerViewId: Int, tag: String,
+        block: (tag: String) -> Fragment
+    ) {
+        val beginTransaction = beginTransaction()
+        for (fragment in fragments) {
+            if (!fragment.isHidden) {
+                beginTransaction.hide(fragment)
+            }
+        }
+        val findFragmentByTag = findFragmentByTag(tag)
+        if (findFragmentByTag == null) {
+            beginTransaction.add(containerViewId, block.invoke(tag), tag)
+        } else {
+            if (findFragmentByTag.isHidden) {
+                beginTransaction.show(findFragmentByTag)
+            }
+        }
+        beginTransaction.commit()
+    }
+
+
     companion object {
+        const val containerViewId = R.id.container
         fun start(context: Context) {
             context.startActivity(Intent(context, MainActivity::class.java))
         }
