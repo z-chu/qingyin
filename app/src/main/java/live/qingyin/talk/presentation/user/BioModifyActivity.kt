d856ab5d32f071b73d38b61a5c03592c7cfb169e
zchu <zchu8073@gmail.com>
2019-03-27 19:46:19 +0800

    [添加]用户登录、用户资料

---
+++ app/src/main/java/live/qingyin/talk/presentation/user/BioModifyActivity.kt
@@ -0,0 +1,16 @@
+package live.qingyin.talk.presentation.user
+
+import android.os.Bundle
+import live.qingyin.talk.R
+import live.qingyin.talk.base.BaseActivity
+
+class BioModifyActivity : BaseActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_modify_bio)
+
+
+    }
+
+}
